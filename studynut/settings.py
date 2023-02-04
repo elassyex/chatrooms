7b8c59427d764dc1ac7603e1e06dc935d1be4963
elassyex <103861551+elassyex@users.noreply.github.com>
2023-02-04 06:50:02 +0300

    Update settings.py

--- studynut/settings.py
+++ studynut/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL ='images/'
+STATIC_URL = '/static/'
+MEDIA_URL ='/images/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
